6d06df4963412cda4f8e4a18250e398ef39be1f9
Lianne Turnbull <lianneturnbull@gmail.com>
2016-10-22 15:13:11 +0100

    Update to use inititalState

--- test/playerStats.spec.js
+++ test/playerStats.spec.js
@@ -1,70 +1,36 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import {expect} from "chai";
 
 import PlayerStats, { StatsSubList, PlayerStatsList } from "../src/display/playerStats";
+import { initialState } from "../src/redux/reducers/playerStats";
 
 describe("<PlayerStatsList />", () => {
-	const playerStats = {
-			name: "Vincent the Unready",
-			class: "mage",
-			level: 1,
-			health: {
-				total: 12,
-				current: 4
-			},
-			resource: {
-				type: "mana",
-				total: 6,
-				current: 3
-			},
-			inventory: [
-				{
-					name: "Health Potion",
-					value: 5
-				},
-				{
-					name: "Yew Wand",
-					value: 1
-				},
-				{
-					name: "Magic Ring of Floating",
-					value: 1
-				}
-			],
-			abilities: [
-				{
-					name: "Whirlwind",
-					value: 5
-				}
-			]
-		},
-		playerStatItems = Object.getOwnPropertyNames(playerStats);
-
-	const wrapper = shallow(<PlayerStatsList playerStats={playerStats} />),
+	const playerStats = initialState,
+		playerStatItems = Object.getOwnPropertyNames(playerStats),
+		wrapper = shallow(<PlayerStatsList playerStats={playerStats} />),
 		listItems = wrapper.children();
+
 	it("should display an unordered list containing player stats", () => {
 		expect(wrapper.is("ul")).to.equal(true);
 		expect(listItems.length).to.equal(playerStatItems.length);
 	});
-	it("should give a key to each item in the list", () => {
-		listItems.forEach((item, index) => {
-			let key = playerStatItems[index];
-			expect(item.key()).to.equal(key);
+	describe("li", () => {
+		it("should have a key naming the stat", () => {
+			listItems.forEach((item, index) => {
+				let key = playerStatItems[index];
+				expect(item.key()).to.equal(key);
+			});
 		});
-	});
-	it("should contain two sub lists - inventory and abilities", () => {
-		expect(wrapper.find(StatsSubList)).to.have.length(2);
-	});
+		describe("<StatsSubList />", () => {
+			it("should output a list of stats from an array of objects", () => {
+				const stat = playerStats.abilities;
+				const wrapper = shallow(<StatsSubList stat={stat} />),
+					listItems = wrapper.children();
 
-	describe("<StatsSubList />", () => {
-		it("should output a list of stats from an array of objects", () => {
-			const stat = playerStats.abilities;
-			const wrapper = shallow(<StatsSubList stat={stat} />),
-				listItems = wrapper.children();
-
-			expect(wrapper.is("ul")).to.equal(true);
-			expect(listItems.length).to.equal(stat.length);
+				expect(wrapper.is("ul")).to.equal(true);
+				expect(listItems.length).to.equal(stat.length);
+			});
 		});
 	});
 });
