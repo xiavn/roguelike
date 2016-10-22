84dc56ab0d573396fc737b82882101e91ed0f69e
Lianne Turnbull <lianneturnbull@gmail.com>
2016-10-22 12:03:43 +0100

    Add character attributes array

--- src/options.js
+++ src/options.js
@@ -1,6 +1,7 @@
 export const classOptions = {
 	classes: ["fighter","mage"],
 	resources: ["mana", "rage"],
+	attributes: ["strength", "speed", "accuracy", "intelligence", "spirit", "constitution"],
 	classStats: {
 		fighter: {
 			resource: "rage",
