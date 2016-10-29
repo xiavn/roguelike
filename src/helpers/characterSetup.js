af50b31c01c7a8c78c9f8e924bcd025fdba5a565
Lianne Turnbull <lianneturnbull@gmail.com>
2016-10-29 18:30:25 +0100

    Generate health and resource derived attributes.

--- src/helpers/characterSetup.js
+++ src/helpers/characterSetup.js
@@ -7,13 +7,10 @@ class Character {
 	constructor(options) {
 		this.class = options.classes[Math.floor(Math.random() * options.classes.length)];
 	}
-	get resource() {
-		return options.classStats[this.class].resource;
-	}
-
 	get combinedAttributes() {
 		const classStats = options.classStats[this.class];
 		let attributes = {};
+		//Generate a set or random attributes: 1d4+1 + class bonus
 		options.attributes.forEach((att) => {
 			let value = diceRoller("1d4 + 1", att);
 			if (classStats.hasOwnProperty(att)) {
@@ -21,15 +18,43 @@ class Character {
 			}
 			attributes[att] = value;
 		});
-		let combined = {
-			attributes: attributes
+		//Generate health: class health die(full value) + constitution
+		const healthDie = /\d+/g.exec(classStats.health);
+		const health = parseInt(healthDie[0]) + attributes.constitution;
+		//Generate resource: resource dependent on class
+		const resource = classStats.resource;
+		let rsTotal,
+			rsCurrent;
+		switch(resource) {
+			case "rage":
+				rsCurrent =  0;
+				rsTotal = 5 + attributes.constitution;
+				break;
+			case "mana":
+				rsTotal = 5 + attributes.intelligence;
+				rsCurrent = rsTotal;
+				break;
+			default:
+				rsTotal = 0;
+				rsCurrent = 0;
+				break;
+		};
+		let derived = {
+			health: {
+				total: health,
+				current: health
+			},
+			resource: {
+				type: resource,
+				total: rsTotal,
+				current: rsCurrent
+			}
 		}
+		let combined = {
+			attributes: attributes,
+			derived: derived
+		};
 		return combined;
-	}
-
-	set health(constitution) {
-		const total = diceRoller(options.classStats[this.class].health, "health") + constitution;
-		return { total: total, current: total};
 	}
 }
 
--- src/redux/reducers/playerStats.js
+++ src/redux/reducers/playerStats.js
@@ -1,20 +1,14 @@
 import { SAVE_NAME, SAVE_CLASS, CHANGE_LEVEL } from "../actions/actionTypes";
 import character from "../../helpers/characterSetup";
 
+const attributes = character.combinedAttributes;
 export const initialState = {
 	name: "Wheezer the Crazy",
 	class: character.class,
 	level: 1,
-	health: {
-		total: 0,
-		current: 0
-	},
-	attributes: character.combinedAttributes.attributes,
-	resource: {
-		type: character.resource,
-		total: 0,
-		current: 0
-	},
+	health: attributes.derived.health,
+	attributes: attributes.attributes,
+	resource: attributes.derived.resource,
 	inventory: [
 		{
 			name: "Health Potion",
@@ -31,15 +25,14 @@ export const initialState = {
 
 export default function playerStats(state = initialState , action) {
 	switch (action.type) {
-	case SAVE_NAME:
-		return  { ...state, name: action.name };
-	case SAVE_CLASS:
-		return { ...state, class: action.pClass };
-	case CHANGE_LEVEL:
-		return {...state, level: state.level+action.by };
+		case SAVE_NAME:
+			return  { ...state, name: action.name };
+		case SAVE_CLASS:
+			return { ...state, class: action.pClass };
+		case CHANGE_LEVEL:
+			return {...state, level: state.level+action.by };
 
-	default:
-		return state;
-
+		default:
+			return state;
 	}
 }
