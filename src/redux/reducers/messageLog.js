144ca3fc97513574804c3c4f908c098a6a2563ed
Lianne Turnbull <lianneturnbull@gmail.com>
2016-11-05 23:15:58 +0000

    Add save-class action to message log reducer

--- src/redux/reducers/messageLog.js
+++ src/redux/reducers/messageLog.js
@@ -1,9 +1,15 @@
-import { SEND_MESSAGE } from "../actions/actionTypes";
+import { SEND_MESSAGE, SAVE_CLASS } from "../actions/actionTypes";
 
 export default function messageLog(state = [], action) {
 	switch (action.type) {
 	case SEND_MESSAGE:
 		return [...state,action.message];
+	case SAVE_CLASS:
+		const message = {
+			type: "newClass",
+			text: `Your class is now ${action.pClass}`
+		};
+		return [ ...state, message ];
 	default:
 		return state;
 	}
