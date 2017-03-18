a735186ad684a9e921e54fcf08d5f7d526998b5f
Lianne Turnbull <lianneturnbull@gmail.com>
2017-03-18 10:11:47 +0000

    Remove old code no longer needed

--- src/helpers/Dungeon/MapMaze.js
+++ src/helpers/Dungeon/MapMaze.js
@@ -9,105 +9,10 @@ export default class Maze extends Map {
 	}
 
 	createMaze() {
-
-		// const countExits = (cell) => {
-		// 	let count = 0;
-		// 	for (let exit in cell.exits) {
-		// 		if (cell.exits[exit]) {
-		// 			count++;
-		// 		}
-		// 	}
-		// 	return count;
-		// };
-
-		// const makeSparse = (count) => {
-		// 	for (let i = 0; i < count; i++) {
-		// 		let deadEnds = [];
-		// 		this.map.forEach((column) => {
-		// 			column.forEach((cell) => {
-		// 				if (countExits(cell) === 1) {
-		// 					deadEnds.push(cell);
-		// 				}
-		// 			});
-		// 		});
-
-		// 		deadEnds.forEach((cell) => {
-		// 			for (let exit in cell.exits) {
-		// 				if (cell.exits[exit]) {
-		// 					cell.type = "rock";
-		// 					cell.exits[exit] = false;
-		// 					let neighbour = this.move(exit, cell);
-		// 					switch(exit) {
-		// 						case 'north':
-		// 							neighbour.exits.south = false;
-		// 							break;
-		// 						case 'south':
-		// 							neighbour.exits.north = false;
-		// 							break;
-		// 						case 'east':
-		// 							neighbour.exits.west = false;
-		// 							break;
-		// 						case 'west':
-		// 							neighbour.exits.east = false;
-		// 							break;
-		// 					}
-		// 				}
-		// 			}
-		// 		});
-		// 	}
-		// };
-
-		// const removeDeadEnds = (chance) => {
-		// 	this.map.forEach((column) => {
-		// 		column.forEach((cell) => {
-		// 			if (countExits(cell) === 1 && diceRoller("d100") < chance) {
-		// 				blocked = [];
-		// 				for (let exit in cell.exits) {
-		// 					if (cell.exits[exit]) {
-		// 						blocked.push(exit);
-		// 					}
-		// 				}
-		// 				this.currentCell = cell;
-		// 				currentDir = this.chooseDirection(blocked);
-		// 				//console.log(cell.location);
-		// 				linkDeadEnd(cell);
-		// 			}
-		// 		});
-		// 	});
-		// };
-
-		// const linkDeadEnd = (start) => {
-		// 	if (blocked.length !== 4) {
-		// 		if (blocked.indexOf(currentDir) === -1) {
-		// 			if (this.checkDirection(currentDir,false)) {
-		// 				this.tunnel(currentDir);
-		// 				if (this.currentCell.type === "floor") {
-		// 					this.currentCell.visit(currentDir);
-		// 				} else {
-		// 					this.currentCell.visit(currentDir);
-		// 					blocked = [];
-		// 					//console.log(`moving ${currentDir}`);
-		// 					linkDeadEnd(start);
-		// 				}
-		// 			} else {
-		// 				//console.log(`can't go ${currentDir}`);
-		// 				blocked.push(currentDir);
-		// 				currentDir = this.chooseDirection(blocked);
-		// 				linkDeadEnd(start);
-		// 			}
-		// 		}
-		// 	}
-		// };
-
-		// const createRoom = () => {
-
-		// };
 		const Dorn = new Dwarf(this, this.chooseCell());
 		Dorn.cell.excavate();
-		//console.log(`current cell: ${this.currentCell.location[0]},${this.currentCell.location[1]}`);
 		Dorn.checkRoute();
 		Dorn.collapseDeadEnds(30);
 		Dorn.connectDeadEnds(80);
-		// removeDeadEnds(80);
 	}
 }
